c4fb7243347b89b4b5c9c266ad030358392107d8
Sean Liu <seanliu96@outlook.com>
2016-12-25 16:13:21 +0800

    2016年12月25日 星期日 16时13分21秒 CST

--- BackEnd/consts.py
+++ BackEnd/consts.py
@@ -2,6 +2,17 @@
 # *_* coding:utf-8 *_*
 
 SERVER_IP = "172.18.71.35"
-#SERVER_IP = "172.19.96.127"
 SERVER_PORT = "8080"
 SERVER_ROOT_URL = "http://" + SERVER_IP + ":" + SERVER_PORT
+
+INDEX_API = SERVER_ROOT_URL + "/"
+LOGIN_API = SERVER_ROOT_URL + "/api/login"
+REGISTER_API = SERVER_ROOT_URL + "/api/register"
+USER_UPDATE_API = SERVER_ROOT_URL + "/api/user/update"
+USER_UPLOAD_API = SERVER_ROOT_URL + "/api/user/upload"
+USER_RESET_API = SERVER_ROOT_URL + "/api/user/reset"
+USER_ADDNOTEBOOK_API = SERVER_ROOT_URL + "/api/user/addnotebook"
+NOTEBOOK_UPDATE_API = SERVER_ROOT_URL + "/api/notebook/update"
+NOTEBOOK_UPLOAD_API = SERVER_ROOT_URL + "/api/notebook/upload"
+NOTEBOOK_ADDNOTE_API = SERVER_ROOT_URL + "/api/notebook/addnote"
+NOTE_UPDATE_API = SERVER_ROOT_URL + "/api/note/update"
---
+++ BackEnd/test.py
@@ -0,0 +1,59 @@
+from urllib import request, parse
+from consts import *
+from datetime import datetime
+
+def register():
+    name = input("name")
+    pwd = input("pwd")
+    data = {
+        "name": name,
+        "pwd": pwd,
+    }
+    data = parse.urlencode(data)
+    req = request.Request(REGISTER_API, data.encode('utf-8'))
+    resp = request.urlopen(req)
+    print(resp.read())
+
+def login():
+    name = input("name")
+    pwd = input("pwd")
+    data = {
+        "name": name,
+        "pwd": pwd,
+    }
+    data = parse.urlencode(data)
+    req = request.Request(REGISTER_API, data.encode('utf-8'))
+    resp = request.urlopen(req)
+    print(resp.read())
+
+def addNotebook():
+    uid = input("uid")
+    name = input("name")
+    data = {
+        "uid": uid,
+        "name": name,
+    }
+    data = parse.urlencode(data)
+    req = request.Request(REGISTER_API, data.encode('utf-8'))
+    resp = request.urlopen(req)
+    print(resp.read())
+
+def addNote():
+    uid = input("uid")
+    nbid = input("nbid")
+    title = input("title")
+    time = datetime.now()
+    weekday = datetime.weekday(time) + 1
+    body = input("body")
+    data = {
+        "uid": uid,
+        "nbid", nbid,
+        "title": name,
+        "time": time,
+        "weekday": weekday,
+        "body": body,
+    }
+    data = parse.urlencode(data)
+    req = request.Request(REGISTER_API, data.encode('utf-8'))
+    resp = request.urlopen(req)
+    print(resp.read())
